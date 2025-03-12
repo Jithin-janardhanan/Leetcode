9463cca38f9283fe4a09c5c224253063d3f0ba15
Jithin-janardhanan <jithinjanardhanan97@gmail.com>
2025-03-13 00:37:39 +0530

    length of last word

---
+++ bin/length_of_last_word.dart
@@ -0,0 +1,14 @@
+class Solution {
+  int lengthOfLastWord(String s) {
+    s = s.trim(); 
+    List<String> words = s.split(" "); 
+    return words.isNotEmpty ? words.last.length : 0; 
+  }
+}
+
+void main() {
+  Solution solution = Solution();
+  print(solution.lengthOfLastWord("Hello World")); 
+  print(solution.lengthOfLastWord("   fly me   to   the moon  ")); 
+  print(solution.lengthOfLastWord("luffy is still joyboy")); 
+}gi
